1d602d4b98bc2c43bb6041baf2b0d0739daf7ddf
YOSUKE TAKEHISA <ytakehisa@MacBook-Pro.local>
2019-01-10 01:53:19 +0900

    Refactoring MemoList

---
+++ src/elements/CircleButton.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { StyleSheet, View, Text } from 'react-native';
+
+class CircleButton extends React.Component {
+  render() {
+    return (
+      <View style={styles.CircleBotton}>
+        <Text style={styles.CircleBottonTitle}>
+          {this.props.children}
+        </Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  CircleBotton: {
+    position: 'absolute',
+    bottom: 32,
+    right: 32,
+    width: 48,
+    height: 48,
+    backgroundColor: '#E31676',
+    borderRadius: 24,
+    justifyContent: 'center',
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.2,
+    shadowRadius: 3,
+  },
+  CircleBottonTitle: {
+    fontSize: 32,
+    lineHeight: 32,
+    color: '#fff',
+  },
+});
+
+export default CircleButton;
