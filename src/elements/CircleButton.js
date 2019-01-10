8492574b8836206dc1b5fa1c07f4bcb7fd4f6fdc
YOSUKE TAKEHISA <ytakehisa@MacBook-Pro.local>
2019-01-10 18:34:53 +0900

    Style MemoDetail Screen

--- src/elements/CircleButton.js
+++ src/elements/CircleButton.js
@@ -3,9 +3,19 @@ import { StyleSheet, View, Text } from 'react-native';
 
 class CircleButton extends React.Component {
   render() {
+    const { style, color } = this.props;
+
+    let bgColor = '#E31676';
+    let textColor = '#fff';
+
+    if (color === 'white') {
+      bgColor = '#fff';
+      textColor = '#E31676';
+    }
+
     return (
-      <View style={styles.CircleBotton}>
-        <Text style={styles.CircleBottonTitle}>
+      <View style={[styles.circleButton, style, { backgroundColor: bgColor }]}>
+        <Text style={[styles.circleButtonTitle, { color: textColor }]}>
           {this.props.children}
         </Text>
       </View>
@@ -14,13 +24,12 @@ class CircleButton extends React.Component {
 }
 
 const styles = StyleSheet.create({
-  CircleBotton: {
+  circleButton: {
     position: 'absolute',
     bottom: 32,
     right: 32,
     width: 48,
     height: 48,
-    backgroundColor: '#E31676',
     borderRadius: 24,
     justifyContent: 'center',
     alignItems: 'center',
@@ -29,10 +38,9 @@ const styles = StyleSheet.create({
     shadowOpacity: 0.2,
     shadowRadius: 3,
   },
-  CircleBottonTitle: {
+  circleButtonTitle: {
     fontSize: 32,
     lineHeight: 32,
-    color: '#fff',
   },
 });
 
