d47c419a2b1728de6a6dce4ee4e46806965d709a
YOSUKE TAKEHISA <ytakehisa@MacBook-Pro.local>
2019-01-11 19:22:29 +0900

    Add icon font

--- src/screens/MemoListScreen.js
+++ src/screens/MemoListScreen.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
 
-
 import MemoList from '../components/MemoList';
 import CircleButton from '../elements/CircleButton';
 
@@ -9,9 +8,8 @@ class MemoListScreen extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-
         <MemoList />
-        <CircleButton>+</CircleButton>
+        <CircleButton name="plus" />
       </View>
     );
   }
